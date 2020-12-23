c3f847be2f6291931052917048c18038d5d06156
Itamar Hoshmand <hoshmy@gmail.com>
2020-12-24 00:03:18 +0200

    First basic functioning server with response

--- api/views.py
+++ api/views.py
@@ -3,11 +3,24 @@ import json
 
 from utilities.similar_words_dictionary_parser import SimilarWordsDictionaryParser
 from configurations import configuration
-
+from utilities.logger import Logger
 
 parser = SimilarWordsDictionaryParser()
-parser.parse(configuration.path_to_dictionary_file)
+similar_words_dictionary = parser.parse(configuration.path_to_dictionary_file)
 
 
 def similar(request):
-    return HttpResponse("Hello, world. You're at the api/v1/similar")
+    if request.method == 'GET':
+        word = request.GET['word']  # TODO: check if missing
+        ret_dict = {'similar': []}
+        Logger.log('query for word {}'.format(word))
+
+        word = word.rstrip('\n').strip().lower()
+        # sorted word will be used as the information key
+        sorted_word = ''.join(sorted(set(word.lower())))
+        if sorted_word in similar_words_dictionary:
+            ret_dict = similar_words_dictionary[sorted_word]
+            Logger.log('found similar word for {}: {}'.format(word, ret_dict))
+
+        json_string = json.dumps(ret_dict)
+        return HttpResponse(json_string)
